664a5a667f8499dc38b79577ced6a87f2b5381ca
TheLPerSteve <steven@wjertzoch.de>
2016-04-12 18:11:16 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+var game = function() {
+	var userChoice = prompt("Choose a number between 1 and 10!");
+	var pcChoice = Math.floor(Math.random() * (10 - 1 + 1)) + 1; 
+
+	console.log(pcChoice);	
+	if(userChoice > 10) {
+		console.log("Please only choose numbers between 1 and 10!");
+	}
+	else {
+		if(userChoice == pcChoice) {
+			console.log("You win!");
+		}
+		else {
+			console.log("The computer chose " + pcChoice + " , but you chose " + userChoice + "." + " Try again!");
+		}
+	}
+}
+
+game();
